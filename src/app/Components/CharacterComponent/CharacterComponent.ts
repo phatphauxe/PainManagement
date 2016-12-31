e7f646c7bcdf998e02399027301e4e2d10f853a7
Heath McKinney <littlebittheatre@gmail.com>
2016-12-30 22:43:28 -0800

    ground work for character builder, class selection and campaign types

--- src/app/Components/CharacterComponent/CharacterComponent.ts
+++ src/app/Components/CharacterComponent/CharacterComponent.ts
@@ -5,12 +5,53 @@ import {Component} from '@angular/core';
 
 @Component ({
   selector: 'character-builder',
-  styles: [],
+  styles: [require('./character-builder.style.css')],
   template: require('./character-builder.template.html')
 })
 export class CharacterBuilder {
+  campaignTypes = [{name: 'Roll for Stats', value: 0}, {name: 'Low Fantasy', value: 10}, {name: 'Standard Fantasy', value: 15}, {name: 'High Fantasy', value: 20}, {name: 'Epic Fantasy', value: 25}];
+  campaignType:Number = 0;
   chars = [{name: 'Barbarian'}, {name: 'Bard'}, {name: 'Cleric'}, {name: 'Druid'}, {name: 'Fighter'}, {name: 'Monk'}, {name: 'Paladin'}, {name: 'Ranger'}, {name: 'Rouge'}, {name: 'Sorcerer'}, {name: 'Wizard'}];
+  userStep:any = 0;
+  character:any = {};
+  stepTitleArray:String[] = ['Select a Class', 'Select your levels'];
   constructor(){
 
+  }
+
+  getActive(step){
+    if(step === this.userStep){
+      return 'active';
+
+    }
+
+    return '';
+  }
+
+  stepTitle(){
+    return this.stepTitleArray[this.userStep];
+  }
+
+  selectClass(char){
+    this.character = char;
+    this.userStep += 1;
+    console.log(char);
+  }
+
+  resetBuild(){
+    this.userStep = 0;
+    this.character = {};
+
+  }
+
+  logChange(e){
+    let select = e.currentTarget;
+    let value = select.selectedOptions[0].value;
+    if(value === this.campaignType){
+      return true;
+    }
+    else {
+      this.campaignType = value;
+    }
   }
 }
